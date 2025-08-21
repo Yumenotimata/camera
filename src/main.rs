3bc1e61868559567024c90c7371e378871e7dcde
Yumenotimata <kouyayamaoka@gmail.com>
2025-08-21 07:34:33 +0100

    k

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,12 @@
 use core::time;
-use std::{vec};
+use std::{u8, vec};
 use std::thread::sleep;
 use file_mmap::FileMmap;
-use image;
 use minifb::{self, Window};
 
 fn display_image(width:usize,height:usize,buffer:*const u8,window:&mut Window){
 
-	//let mut img = image::RgbImage::new(width as u32,height as u32);
-	//unsafe {ptr::copy(buffer,img.as_mut_ptr(),width * height * 3);}
-	//img.save("mine.png").unwrap();
-	let mut image_data:Vec<u32> = vec::Vec::with_capacity(width*height);
+	let mut image_data:Vec<u32> = vec![0;width*height];
 	for (index,c) in unsafe {std::slice::from_raw_parts(buffer, width * height * 3)}.iter().enumerate(){
 		let pixel_index = index / 3;
 		image_data[pixel_index] = (image_data[pixel_index] << 8) | *c as u32;
@@ -24,6 +20,12 @@ fn display_image(width:usize,height:usize,buffer:*const u8,window:&mut Window){
 fn main()  {
 	let camera_width = 4056;
 	let camera_height = 3040;
+	let image_width = 100;
+	let image_height = 100;
+
+	let ipix_per_cpix_w = camera_width / image_width;
+	let ipix_per_cpix_h = camera_height / image_height;
+
 	let mut fm=FileMmap::new("./pycamera2_frame").unwrap();
 	fm.set_len(camera_width * camera_height * 3 + 1).unwrap();
 	println!("Hello");
@@ -31,18 +33,41 @@ fn main()  {
 
 	let mut window = minifb::Window::new(
 	"Minifb Image Viewer",
-	camera_width as usize,
-	camera_height as usize,
+	image_width as usize,
+	image_height as usize,
 	minifb::WindowOptions::default(),
 	).unwrap();
 	
+	
 	let mut last_index = 0;
 	loop {
-		let p = fm.as_ptr();
+		let mut p = fm.as_ptr();
 		if last_index != unsafe{p.read_unaligned()} {
 			last_index = unsafe{p.read_unaligned()};
 			println!("changed!");
-			display_image(camera_width as usize, camera_height as usize, unsafe{p.add(1)},&mut window);
+
+			let mut image_sum:Vec<u64> = vec![0;(image_width*image_height * 3 )as usize];
+			for i in 0..(camera_width*camera_height*3)as u64{
+				let color_index = i % 3;
+				let pixel_index
+				let x = (i % camera_width) / ipix_per_cpix_w as u64;
+				let y = (i / camera_width) / ipix_per_cpix_h as u64;
+				p = unsafe{p.add(1)};
+				image_sum[(x + image_width as u64 * y) as usize] += unsafe{*p} as u64;
+				println!("{:?},{:?}",x,y);
+			}
+
+
+			let mut image:Vec<u8> = vec![0;(image_width*image_height * 3) as usize];
+			for (i,e) in image_sum.iter().enumerate(){
+				image[i] = (*e / (ipix_per_cpix_w * ipix_per_cpix_h)) as u8;
+				//println!("{:?}",*e);
+
+			}
+
+			//println!("{:?}",image);
+
+			display_image(image_width as usize,image_height as usize, image.as_ptr(),&mut window);
 		}
 
 		sleep(time::Duration::from_millis(1000));
