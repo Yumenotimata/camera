400c845841a7e3af495457eaba47fd3b3dc4c7ea
Yumenotimata <kouyayamaoka@gmail.com>
2025-08-21 06:48:56 +0100

    j

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,25 @@
 use core::time;
-use std::path::{Path, PathBuf};
-use std::{fs, ptr};
+use std::{vec};
 use std::thread::sleep;
 use file_mmap::FileMmap;
 use image;
-use minifb;
+use minifb::{self, Window};
+
+fn display_image(width:usize,height:usize,buffer:*const u8,window:&mut Window){
+
+	//let mut img = image::RgbImage::new(width as u32,height as u32);
+	//unsafe {ptr::copy(buffer,img.as_mut_ptr(),width * height * 3);}
+	//img.save("mine.png").unwrap();
+	let mut image_data:Vec<u32> = vec::Vec::with_capacity(width*height);
+	for (index,c) in unsafe {std::slice::from_raw_parts(buffer, width * height * 3)}.iter().enumerate(){
+		let pixel_index = index / 3;
+		image_data[pixel_index] = (image_data[pixel_index] << 8) | *c as u32;
+	}
+
+	window.update_with_buffer(&image_data, width,height).unwrap();
+
+
+}
 
 fn main()  {
 	let camera_width = 4056;
@@ -12,6 +27,14 @@ fn main()  {
 	let mut fm=FileMmap::new("./pycamera2_frame").unwrap();
 	fm.set_len(camera_width * camera_height * 3 + 1).unwrap();
 	println!("Hello");
+
+
+	let mut window = minifb::Window::new(
+	"Minifb Image Viewer",
+	camera_width as usize,
+	camera_height as usize,
+	minifb::WindowOptions::default(),
+	).unwrap();
 	
 	let mut last_index = 0;
 	loop {
@@ -19,13 +42,8 @@ fn main()  {
 		if last_index != unsafe{p.read_unaligned()} {
 			last_index = unsafe{p.read_unaligned()};
 			println!("changed!");
+			display_image(camera_width as usize, camera_height as usize, unsafe{p.add(1)},&mut window);
 		}
-
-		let mut img = image::RgbImage::new(camera_width as u32,camera_height as u32);
-		unsafe {ptr::copy(p.add(1),img.as_mut_ptr(),(camera_width * camera_height * 3) as usize);}
-
-
-		//img.save("mine.png").unwrap();
 
 		sleep(time::Duration::from_millis(1000));
 	}
